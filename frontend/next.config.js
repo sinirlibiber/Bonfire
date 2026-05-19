15aff2c4bcb6a3818c0a382dcc6d7b1764b6df17
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2026-05-19 22:17:04 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      "pino-pretty": false,
+      "@react-native-async-storage/async-storage": false,
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
